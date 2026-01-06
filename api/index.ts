2c26486b1a4ac2df2b65b1b7b1cbab7e5aac39fd
guih0113 <gui.henriquess13@gmail.com>
2026-01-06 00:16:44 -0300

    fix: deploy configs

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
-import { app } from '../src/server'
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { app } from '../src/server.js'
 
-export default async (req: Request, res: Response) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
   await app.ready()
   app.server.emit('request', req, res)
 }
